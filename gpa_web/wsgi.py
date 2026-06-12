9be179154361f6833c99e7968a69fedf123e2354
ngochoaphan2004 <phanngochoa2004@gmail.com>
2026-06-12 23:15:56 +0700

    temp

--- gpa_web/wsgi.py
+++ gpa_web/wsgi.py
@@ -1,19 +1,9 @@
-"""
-WSGI config for gpa_web project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gpa_web.settings')
 
 application = get_wsgi_application()
 
-# BẮT BUỘC PHẢI CÓ DÒNG NÀY CHO VERCEL
+# BẮT BUỘC PHẢI CÓ DÒNG NÀY ĐỂ VERCEL NHẬN DIỆN ENTRY POINT
 app = application
